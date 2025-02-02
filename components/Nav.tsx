79ba3fc4b0276151c6f9c05d82d293ebd75cf91b
rasyidannas <rasshit.dsgn@gmail.com>
2025-02-02 10:59:48 +0700

    Part 5.mp4 - Non form server actions

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,32 @@
 'use client'
 import { Input } from '@nextui-org/react'
+import { createNewEvent } from '@/actions/events'
 import { Button, Tooltip } from '@nextui-org/react'
 import { CirclePlus } from 'lucide-react'
+import { useTransition } from 'react'
 
 const Nav = () => {
+  const [isPending, startTransition] = useTransition()
+  
+  const handleClick = () => {
+    startTransition(() => {
+      createNewEvent()
+    })
+  }
+
   return (
     <nav className="h-[65px] border-b border-default-50 flex items-center px-6 gap-4">
+      <Tooltip>
+        <Button
+          isIconOnly
+          variant="ghost"
+          size="sm"
+          isLoading={isPending}
+          onClick={handleClick}
+        >
+          <CirclePlus size={10} />
+        </Button>
+      </Tooltip>
       <div className="w-1/2">
         <Input size="sm" variant="faded" placeholder="search" />
       </div>
